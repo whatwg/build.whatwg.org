whatwg/build.whatwg.org

commit 549e077e7d004e11e3518f6cdb42ddbd18e2ebb3

Various improvements to the /wattsi endpoint
In the course of working on adding a full /html-build endpoint, a few improvements came up which we can apply ahead of time to just the /wattsi endpoint:

* Improve README documentation.
* Abstract out the code for turning query parameters into boolean arguments.
* Use more modern JS features.
* Put temporary directories inside the OS temp directory.
* Add a new helper for spawning a command-line program while capturing its output. This fixes #3.
* Perform cleanup file deletions in parallel and log errors if they occur while deleting.
* Move from deprecated --production npm install argument to modern --omit=dev.

lib/app.js
@@ -1,12 +1,14 @@
 "use strict";
+const path = require("path");
+const os = require("os");
+const childProcess = require("child_process");
+const { promisify } = require("util");
+const execFile = promisify(childProcess.execFile);
+const { createReadStream } = require("fs");
+const { mkdir, rm, writeFile } = require("fs").promises;
 const Koa = require("koa");
 const KoaRouter = require("koa-router");
 const { koaBody } = require("koa-body");
-const path = require("path");
-const { promisify } = require("util");
-const execFile = promisify(require("child_process").execFile);
-const { createReadStream } = require("fs");
-const { mkdir, rm, unlink, writeFile } = require("fs").promises;
 const finished = require("finished");
 
 const app = new Koa();
@@ -28,58 +30,50 @@ const bodyParser = koaBody({
 });
 
 router.post("/wattsi", bodyParser, async ctx => {
-  const quiet = "quiet" in ctx.request.query;
-  const singlePageOnly = "single-page-only" in ctx.request.query;
+  const booleanArgs = booleanArgsFromQuery(ctx.request.query, ["quiet", "single-page-only"]);
 
   const sha = ctx.request.body.sha || "(sha not provided)";
   const buildType = ctx.request.body.build || "default";
 
-  const sourceFilePath = (ctx.request.files.source && ctx.request.files.source.filepath) ||
-                         ctx.throw(400, "Expected a source file");
-  const mdnFilePath = (ctx.request.files.mdn && ctx.request.files.mdn.filepath) ||
-                      ctx.throw(400, "Expected a mdn file");
+  const sourceFilePath = ctx.request.files.source?.filepath ?? ctx.throw(400, "Expected a source file");
+  const mdnFilePath = ctx.request.files.mdn?.filepath ?? ctx.throw(400, "Expected a mdn file");
 
-  const outDirectory = randomDirectoryName();
+  const outDirectory = newTempDirectoryName();
   await mkdir(outDirectory, { recursive: true });
 
-  const args = [sourceFilePath, sha, outDirectory, buildType, mdnFilePath];
-  if (singlePageOnly) {
-    args.unshift("--single-page-only");
-  }
-  if (quiet) {
-    args.unshift("--quiet");
-  }
+  const args = [sourceFilePath, sha, outDirectory, buildType, mdnFilePath, ...booleanArgs];
 
   try {
     try {
       console.log(`Running wattsi ${args.join(" ")}`);
-      const result = await execFile("wattsi", args);
+      const result = await promisedSpawnWhileCapturingOutput("wattsi", args);
 
       const outputFile = path.join(outDirectory, "output.txt");
-      await writeFile(outputFile, `${result.stdout}\n\n${result.stderr}`, { encoding: "utf-8" });
+      await writeFile(outputFile, result, { encoding: "utf-8" });
       console.log(`  wattsi succeeded`);
     } catch (e) {
-      if (e.stdout) {
-        e.message = `${e.stdout}\n\n${e.stderr}`;
-      }
-      console.log(`  wattsi or file-writing failed: ${e.code}`);
+      const errorBody = e.output ?? e.stack;
+      console.log(`  html-build or file-writing failed:`);
+      console.log(errorBody);
       const headers = typeof e.code === "number" ? { "Wattsi-Exit-Code": e.code } : {};
-      ctx.throw(400, e.message, { headers });
+      ctx.throw(400, errorBody, { headers });
     }
 
     ctx.response.set("Wattsi-Exit-Code", "0");
     const zipFilePath = `${outDirectory}.zip`;
     console.log(`  zipping result`);
-    await execFile("7za", ["a", "-tzip", "-r", zipFilePath, `./${outDirectory}/*`]);
+    await execFile("7za", ["a", "-tzip", "-r", zipFilePath, `${outDirectory}/*`]);
     console.log(`  zipping succeeded`);
 
     ctx.response.type = "application/zip";
     ctx.response.body = createReadStream(zipFilePath);
 
-    finished(ctx, () => unlink(zipFilePath));
+    finished(ctx, () => rm(zipFilePath));
   } finally {
-    await removeAllFiles(ctx);
-    await rm(outDirectory, { recursive: true });
+    await cleanupLoggingRejections([
+      ...requestFinalRemovalPromises(ctx.request),
+      rm(outDirectory, { recursive: true })
+    ]);
   }
 });
 
@@ -88,11 +82,57 @@ app
   .use(router.allowedMethods())
   .listen(process.env.PORT);
 
-function randomDirectoryName() {
-  return Math.random().toString(36).slice(2);
+function newTempDirectoryName() {
+  return path.resolve(os.tmpdir(), Math.random().toString(36).slice(2));
 }
 
-function removeAllFiles(ctx) {
-  const filePaths = Object.values(ctx.request.files).map(file => file.filepath);
-  return Promise.all(filePaths.map(filePath => unlink(filePath)));
+function requestFinalRemovalPromises(request) {
+  const filePaths = Object.values(request.files).map(file => file.filepath);
+  return filePaths.map(filePath => rm(filePath));
+}
+
+function booleanArgsFromQuery(query, possibleArgs) {
+  return possibleArgs.filter(arg => arg in query).map(arg => `--${arg}`);
+}
+
+function promisedSpawnWhileCapturingOutput(...args) {
+  return new Promise((resolve, reject) => {
+    const subprocess = childProcess.spawn(...args);
+
+    let output = "";
+    subprocess.stdout.on("data", data => {
+      output += data;
+    });
+    subprocess.stderr.on("data", data => {
+      output += data;
+    });
+
+    subprocess.on("close", code => {
+      if (code !== 0) {
+        const error = new Error("Process returned nonzero exit code");
+        error.code = code;
+        error.output = output;
+        reject(error);
+      } else {
+        resolve(output);
+      }
+    });
+
+    subprocess.on("error", () => {
+      reject(new Error("Process failed with error event"));
+    });
+  });
+}
+
+async function cleanupLoggingRejections(promises) {
+  const results = await Promise.allSettled(promises);
+  const rejections = results.filter(result => result.status === "rejected");
+
+  if (rejections.length > 0) {
+    const plural = rejections.length === 1 ? "" : "s";
+    console.log(`  Cleanup error${plural}:`);
+    for (const result of rejections) {
+      console.log(`  ${result.reason.stack}`);
+    }
+  }
 }

Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18.17.0-bookworm-slim
+FROM node:18.17.1-bookworm-slim
 RUN apt-get update && \
     apt-get install --yes --no-install-recommends p7zip-full && \
     rm -rf /var/lib/apt/lists/*
@@ -9,7 +9,7 @@ WORKDIR /app
 
 COPY . .
 
-RUN npm install --production
+RUN npm install --omit=dev
 
 ENV PORT=3000
 

commit e042f0a787ed03147d1ba866e1dd314a311cfb53

Fix passing boolean args to Wattsi
This regressed in 549e077e7d004e11e3518f6cdb42ddbd18e2ebb3.

lib/app.js
@@ -41,7 +41,7 @@ router.post("/wattsi", bodyParser, async ctx => {
   const outDirectory = newTempDirectoryName();
   await mkdir(outDirectory, { recursive: true });
 
-  const args = [sourceFilePath, sha, outDirectory, buildType, mdnFilePath, ...booleanArgs];
+  const args = [...booleanArgs, sourceFilePath, sha, outDirectory, buildType, mdnFilePath];
 
   try {
     try {